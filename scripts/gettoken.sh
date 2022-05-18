a280a4d7ebe0f412ff445fafd04da514aa1a94e5
Marcelo Ohashi <mohashi@redhat.com>
2022-05-17 21:37:40 -0300

    Updating WASM

--- scripts/gettoken.sh
+++ scripts/gettoken.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl "$1/admin/api/services/$2/proxy/configs/production/latest.json?access_token=7e06685d5ca405aa675f5e328c470e38edc86d24d39b64d9ad0d064c9279b057" | jq '.proxy_config.content.backend_authentication_value'
+TOKEN=aa50e73eef893e401cbbe89c4c23c554cf46acfe0432214bef5e5f2e66b953ca
+
+curl "$1/admin/api/services/$2/proxy/configs/production/latest.json?access_token=$TOKEN" | jq '.proxy_config.content.backend_authentication_value'
